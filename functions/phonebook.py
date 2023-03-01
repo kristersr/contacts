b1ed5d1cb2fe7060a17ae7c44e7a33c75e6ebec3
kristersr <114050814+kristersr@users.noreply.github.com>
2023-03-01 09:15:05 +0200

    The first version of the app

---
+++ functions/phonebook.py
@@ -0,0 +1,35 @@
+import json
+
+contacts = []
+
+def PrintContacts():
+    for contact in contacts:
+        print(f"{contact['name']} {contact['surname']} {contact['number']} {contact['email']}")
+
+def AddContact():
+    name = input('Enter name: ')
+    surname = input('Enter surname: ')
+    number = input('Enter number: ')
+    email = input('Enter email: ')
+
+    contact = {
+        'name': name, 
+        'surname': surname, 
+        'number': number,
+        'email': email
+    }
+
+    contacts.append(contact)
+
+def SaveContacts():
+    with open('contacts.json', 'w', encoding='UTF8') as file:
+        json.dump(contacts, file, indent=4)
+
+while(True):
+    response = input('A-add new, P-print, E-exit: ')
+    if response == 'A':        
+        AddContact()
+    elif response == 'P':
+        PrintContacts()
+    elif response == 'E':
+        break
